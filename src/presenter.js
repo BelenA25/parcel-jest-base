6a7e9edded7b47cbc9bc9f34c705673e90467341
“Belen <beckiar23@gmail.com>
2023-02-15 21:41:52 -0400

    Multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,28 @@
+import multiplicar from "./multiplicador";
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const first1 = document.querySelector("#primer-numero1");
+const second1 = document.querySelector("#segundo-numero1");
+const form1 = document.querySelector("#sumar-form");
+const form2 = document.querySelector("#multiplicar-form");
+const div1 = document.querySelector("#resultado-div-suma");
+const div2 = document.querySelector("#resultado-div-multiplicar");
 
-form.addEventListener("submit", (event) => {
+form1.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div1.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first1.value);
+  const secondNumber = Number.parseInt(second1.value);
+
+  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
